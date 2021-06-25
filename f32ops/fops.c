f502d8ac5b66ef71ebd520b5d5c5c95d2aa18f22
Lucas Merenciano Martinez <lucasmermar@gmail.com>
2021-06-25 15:34:48 +0200

    Fixed addition and added subtraction

--- f32ops/fops.c
+++ f32ops/fops.c
@@ -16,11 +16,88 @@ inline static uint32_t M(uint32_t a)
     return (uint32_t)a & ((1 << 23) - 1);
 }
 
-static uint32_t f32mul(uint32_t a, uint32_t b)
+static uint32_t f32add(uint32_t ia, uint32_t ib)
+{
+    if ((ia & ((uint32_t)(1 << 31) - 1)) < (ib & ((uint32_t)(1 << 31) - 1)))
+    {
+        uint32_t tmp = ia;
+        ia = ib;
+        ib = tmp;
+    }
+
+    uint32_t rs = S(ia);
+    uint32_t re = E(ia);
+
+    uint32_t exp_dif = re - E(ib);
+    uint32_t ma = M(ia);
+    uint32_t mb = M(ib);
+    uint32_t rm = 0;
+
+    ma |= (1 << 23);
+    mb |= (1 << 23);
+    mb >>= exp_dif;
+
+    if (S(ia) == S(ib))
+    {
+        rm = ma + mb;
+    }
+    else
+    {
+        rm = ma - mb;
+    }
+
+    // Normalize for addition
+    if (rm & (1 << 24))
+    {
+        rm = rm >> 1;
+        re++;
+    }
+
+    // Normalize for subtraction
+    while (!(rm & (1 << 23)))
+    {
+        rm = rm << 1;
+        re--;
+    }
+    
+    uint32_t result = rm & ((1 << 23) - 1);
+    result |= re << 23;
+    result |= rs << 31;
+
+    return result;
+}
+
+static float wf32add(float a, float b)
 {
     uint32_t ia = *(uint32_t*)&a;
     uint32_t ib = *(uint32_t*)&b;
+    uint32_t ires = f32add(ia, ib);
+    return *(float*)&ires;
+}
 
+static uint32_t f32sub(uint32_t ia, uint32_t ib)
+{
+    if (ib & (1<<31))
+    {
+        ib &= ((uint32_t)(1<<31)-1);
+    }
+    else
+    {
+        ib |= (1<<31);
+    }
+    return f32add(ia, ib);
+}
+
+static float wf32sub(float a, float b)
+{
+    uint32_t ia = *(uint32_t*)&a;
+    uint32_t ib = *(uint32_t*)&b;
+    uint32_t ires = f32sub(ia, ib);
+    return *(float*)&ires;
+}
+
+static uint32_t f32mul(uint32_t ia, uint32_t ib)
+{
     uint32_t rs = S(ia) ^ S(ib);
     uint32_t re = E(ia) + E(ib) - 127;
 
@@ -53,22 +130,22 @@ static float wf32mul(float a, float b)
 
 int main(int argc, char **argv)
 {
-    float a1 = -0.3f;
-    float a2 = 99.999f;
-    float a3 = 1.3034f;
+    float a1 = 2.244f;
+    float a2 = 2.54f;
+    float a3 = 42.3034f;
 
-    float b1 = 2.3f;
-    float b2 = -99.999f;
-    float b3 = 0.0f;
+    float b1 = 3.48f;
+    float b2 = -22.2f;
+    float b3 = 12.1f;
 
-    printf("Result 1: %f * %f = %f\n", a1, b1, a1*b1);
-    printf("My Result 1: %f * %f = %f\n", a1, b1, wf32mul(a1, b1));
+    printf("Result 1: %f - %f = %f\n", a1, b1, a1-b1);
+    printf("My Result 1: %f - %f = %f\n", a1, b1, wf32sub(a1, b1));
 
-    printf("Result 2: %f * %f = %f\n", a2, b2, a2*b2);
-    printf("My Result 2: %f * %f = %f\n", a2, b2, wf32mul(a2, b2));
+    printf("Result 2: %f - %f = %f\n", a2, b2, a2-b2);
+    printf("My Result 2: %f - %f = %f\n", a2, b2, wf32sub(a2, b2));
 
-    printf("Result 3: %f * %f = %f\n", a3, b3, a3*b3);
-    printf("My Result 3: %f * %f = %f\n", a3, b3, wf32mul(a3, b3));
+    printf("Result 3: %f - %f = %f\n", a3, b3, a3-b3);
+    printf("My Result 3: %f - %f = %f\n", a3, b3, wf32sub(a3, b3));
 
     return 0;
 }
