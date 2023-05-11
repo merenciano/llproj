3ad54010941e3ddf30889b89094b6ae29aa8cd8a
Lucas Merenciano <lucasmermar@gmail.com>
2023-05-11 22:05:11 +0200

    simple wave

--- pysynth/sine.py
+++ pysynth/sine.py
@@ -3,30 +3,45 @@ import synth
 import dearpygui.dearpygui as dpg
 import dearpygui.demo as demo 
 
-
-
-def config_changed(sender, data):
-    print(data)
-    if data == True:
+def mute(sender, data):
+    if data:
         synth.volume = 0.0
     else:
         synth.volume = 1.0
 
+def freq_changed(sender, data):
+    synth.set_tone(data)
+
+def bend(sender, data):
+    synth.bend(data)
+
+def set_volume(sender, data):
+    synth.volume = data
+
+def update():
+    dpg.set_value("plot", [synth.plot_x, synth.plot_y])
+
 dpg.create_context()
-dpg.create_viewport(title='Jejej', width=1920, height=1080)
+dpg.create_viewport(title='Sinte', width=1920, height=1080)
 
-with dpg.window(tag='Wave nene'):
-    dpg.add_text('K pasaaa')
-    dpg.add_checkbox(label="Mute", default_value=True, callback=config_changed)
+with dpg.window() as primary_window:
+    dpg.add_checkbox(label="Mute", default_value=False, callback=mute)
+    dpg.add_slider_float(label="Volume", default_value=0.5, min_value=0.0 ,max_value=1.0, format="ratio = %.2f", callback=set_volume)
+    dpg.add_input_int(label="Freq", default_value=440, min_value=20, max_value=3000, callback=freq_changed)
+    dpg.add_slider_int(label="Bending", default_value=0, min_value=-60 ,max_value=60, callback=bend)
+    with dpg.plot(label="Wave inspector", height=400, width=-1):
+        dpg.add_plot_axis(dpg.mvXAxis, label="samples")
+        with dpg.plot_axis(dpg.mvYAxis, label="y"):
+            dpg.add_line_series(synth.plot_x, synth.plot_y, tag="plot")
 
+dpg.set_primary_window(primary_window, True)
 demo.show_demo()
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
 
-dpg.set_primary_window("Wave nene", True)
 while dpg.is_dearpygui_running():
+    update()
     dpg.render_dearpygui_frame()
 
 dpg.destroy_context()
-
--- pysynth/synth.py
+++ pysynth/synth.py
@@ -1,34 +1,57 @@
 import numpy as np
 import pyaudio
 
+_wave_inspector_buffers = 200
+_buffer_samples = 100
+
 _p = pyaudio.PyAudio()
 _fs = 44100
-_f = 440.0
+_f = 440
+_f_base = _f
+_bend = 0
 _phase = 0
-_current_vol = 0.0
-volume = 0.0
+_current_vol = 1.0
+volume = 1.0
 
-def _callback(in_data, frame_count, time_info, status):
+plot_x = list(np.linspace(0, _buffer_samples * _wave_inspector_buffers, _buffer_samples * _wave_inspector_buffers))
+plot_y = list(np.linspace(0, 0, _buffer_samples * _wave_inspector_buffers))
+
+def _gen_wave_samples(in_data, frame_count, time_info, status):
+    global plot_y
     global _phase
     global _current_vol
-    waveData = []
-    #waveData = np.sin(_f * 2 * np.pi * (np.arange(_phase, _phase + frame_count) % _fs) / _fs).astype(np.float32)
-    volumeData = np.linspace(_current_vol, volume, frame_count)
-    print(volumeData)
+    wave_data = []
+    volume_data = np.linspace(_current_vol, volume, frame_count)
+
     for i in range(frame_count):
-        waveData.append(np.sin(_f * 2 * np.pi * (_phase / _fs)).astype(np.float32))
+        wave_value = np.sin(_f * 2 * np.pi * (_phase / _fs));
+        wave_value *= volume_data[i];
+        wave_data.append(wave_value.astype(np.float32))
         _phase += 1
         _phase %= _fs
 
+        plot_y.append(wave_value.astype(np.float32))
+        plot_y.pop(0)
+
     _current_vol = volume
-    out = np.asarray(waveData).tobytes()
+    out = np.asarray(wave_data).tobytes()
     return (out, pyaudio.paContinue)
 
-def set_tone(hz):
+def _set_f():
     global  _phase
     global  _f
-    _phase *= (_f / hz)
-    _f = hz 
+    _phase = int(float(_phase) * (_f / (_f_base + _bend)))
+    _f = int(_f_base + _bend)
 
-stream = _p.open(format=pyaudio.paFloat32, channels=1, rate=_fs, frames_per_buffer = 100, stream_callback = _callback, output=True)
+def set_tone(hz):
+    global _f_base
+    _f_base = int(hz)
+    _set_f()
+
+def bend(hz):
+    global _bend
+    _bend = int(hz)
+    _set_f()
+
+stream = _p.open(format=pyaudio.paFloat32, channels=1, rate=_fs, frames_per_buffer = _buffer_samples, stream_callback = _gen_wave_samples, output=True)
 stream.start_stream()
